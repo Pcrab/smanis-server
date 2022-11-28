19135384cda421f613f5162d6c6fae0d881925a6
Pcrab <pcrab@pcrab.xyz>
2022-11-28 12:11:57 +0800

    feat: add jwt and argon2 lib

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import * as dotenv from "dotenv";
+import { encryptPwd, signJwt, verifyJwt, verifyPwd } from "./utils/crypto.js";
 import initDb from "./utils/db.js";
 
 // Init env
@@ -8,10 +9,19 @@ import initDb from "./utils/db.js";
 dotenv.config();
 // console.log(process.env.DB_URL);
 
+// Test JWT
+const jwt = signJwt({ payload: "payload" });
+verifyJwt(jwt);
+// Test PWD
+const hash = await encryptPwd("password");
+if (!(await verifyPwd(hash, "password"))) {
+    throw "pwd error";
+}
+
 // Connecting to MongoDB
 await initDb();
 
-const typeDefs = `
+const typeDefs = `#graphql
 type Book {
     title: String
     author: String
