162a560e4c0ba98b46d19c516ab7dadf2d444af6
Pcrab <pcrab@pcrab.xyz>
2022-12-03 13:12:19 +0800

    feat(backend): add auth

--- packages/backend/src/routes/admin/index.ts
+++ packages/backend/src/routes/admin/index.ts
@@ -1,8 +1,13 @@
 import { FastifyPluginCallback } from "fastify";
+import { auth, AuthLevel } from "../../utils/auth.js";
+import registerRoutes from "../../utils/registerRoutes.js";
 
-const route: FastifyPluginCallback = (_fastify, _opts, done) => {
+const route: FastifyPluginCallback = (fastify, _opts, done) => {
+    auth(fastify, AuthLevel.admin);
+    registerRoutes(fastify, []);
+
+    // Finish
     done();
-    return;
 };
 
 export default route;
--- packages/backend/src/routes/student/index.ts
+++ packages/backend/src/routes/student/index.ts
@@ -1,7 +1,9 @@
 import { FastifyPluginCallback } from "fastify";
+import { auth, AuthLevel } from "../../utils/auth.js";
 import registerRoutes from "../../utils/registerRoutes.js";
 
 const route: FastifyPluginCallback = (fastify, _opts, done) => {
+    auth(fastify, AuthLevel.student);
     registerRoutes(fastify, []);
 
     // Finish
