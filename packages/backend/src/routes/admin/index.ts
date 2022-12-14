81024a7790f7413d247beba98d1b508a9aca6e16
Pcrab <pcrab@pcrab.xyz>
2022-12-14 16:28:20 +0800

    fix(backend): forget add route to index

--- packages/backend/src/routes/admin/index.ts
+++ packages/backend/src/routes/admin/index.ts
@@ -4,10 +4,11 @@ import registerRoutes from "../../utils/registerRoutes.js";
 import modify from "./modify.js";
 import changeStudent from "./changeStudent.js";
 import students from "./listStudents.js";
+import submitExam from "./submitExam.js";
 
 const route: FastifyPluginCallback = (fastify, _opts, done) => {
     auth(fastify, AuthLevel.admin);
-    registerRoutes(fastify, [modify, changeStudent, students]);
+    registerRoutes(fastify, [modify, changeStudent, students, submitExam]);
 
     // Finish
     done();
