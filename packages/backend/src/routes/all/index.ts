2edeaf8b8351688136e9334ed99669c4e3661e8d
Pcrab <pcrab@pcrab.xyz>
2023-01-10 19:11:31 +0800

    fix(backend): fix unregistered adminLogin

--- packages/backend/src/routes/all/index.ts
+++ packages/backend/src/routes/all/index.ts
@@ -1,9 +1,10 @@
 import { FastifyPluginCallback } from "fastify";
 import registerRoutes from "../../utils/registerRoutes.js";
 import loginStudent from "./loginStudent.js";
+import loginAdmin from "./loginAdmin.js";
 
 const route: FastifyPluginCallback = (fastify, _opts, done) => {
-    registerRoutes(fastify, [loginStudent]);
+    registerRoutes(fastify, [loginStudent, loginAdmin]);
 
     // Finish
     done();
